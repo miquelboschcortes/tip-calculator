467ed039360da6fd38d4752416f384286fbd0fef
Miquel Bosch <miquel.bosch.cortes@playsportsnetwork.com>
2023-10-25 00:23:39 +0200

    add result view - shadow extension

---
+++ tip-calculator/Extensions/UIView+Extension.swift
@@ -0,0 +1,24 @@
+//
+//  UIView+Extension.swift
+//  tip-calculator
+//
+//  Created by Miguel Bosch Cortés on 25/10/2023.
+//
+
+import UIKit
+
+extension UIView {
+    
+    func addShadow(offset: CGSize, color: UIColor, radius: CGFloat, opacity: Float) {
+        layer.cornerRadius = radius
+        layer.masksToBounds = false
+        layer.shadowOffset = offset
+        layer.shadowColor = color.cgColor
+        layer.shadowRadius = radius
+        layer.shadowOpacity = opacity
+        
+        let backgroundCGColor = backgroundColor?.cgColor
+        backgroundColor = nil
+        layer.backgroundColor = backgroundCGColor
+    }
+}
